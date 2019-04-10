c99a88fbf1c70186f2f7da7d976ba1ee8247a390
Kayla <kholman@mit.edu>
2019-04-10 15:37:32 -0400

    Created files for graph builder and search algirithm

---
+++ src/search.py
@@ -0,0 +1,9 @@
+# Using A* algorithm searches graph made in graph_builder.py to find
+# shortest path from starting position to ending position
+
+start = "starting node in graph"
+goal = "ending node in graph"
+
+
+frontier = []
+frontier[0] = start
