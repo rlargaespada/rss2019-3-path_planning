482f78dc17cdfe17cedd6402a2abc8a8f74806e4
Kayla <kholman@mit.edu>
2019-04-16 09:42:57 -0400

    changed search to work with graph interface

--- src/search.py
+++ src/search.py
@@ -5,24 +5,27 @@ start = "starting node in graph"
 goal = "ending node in graph"
 graph = "whatever data structure we are using for graph"
 
+
 def a_star(graph, start, goal):
 	frontier = []
 	frontier.append((start,0)) # frontier consists of nodes 
 	came_from = {}
-	cost = {}
+	current_cost = {}
 	came_from[start] = None
-	cost[start] = 0
+	current_cost[start] = 0
 
 	while len(frontier)!=0:
 		current = frontier.pop(0)
 		if current == goal:
 			break
 
-		for n in graph[current].neighbors():
-			new_cost = cost[current] + graph.cost(current, n)
-			if n not in cost or new_cost<cost[n]:
-				cost[n] = new_cost
-				priority = new_cost + heuristic(goal, n)
+		neighbors = graph.edges[current] # list of edges
+		for edge in neighbors:
+			n = edge.end
+			new_cost = current_cost[current] + edge.cost
+			if n not in current_cost or new_cost<current_cost[n]:
+				current_cost[n] = new_cost
+				priority = new_cost + graph.heuristic(goal, n)
 				frontier = queue_insert(frontier, n, priority)
 
 				came_from[n] = current
