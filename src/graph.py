83fe1f6f144c5ac87e38f607ed62a40806011c2e
rlargaes <rlargaes@mit.edu>
2019-04-16 09:31:11 -0400

    added basic graph building, needs to be optimized

--- src/graph.py
+++ src/graph.py
@@ -1,9 +1,16 @@
 class Node(object):
-	def __init__(self,x,y):
+	def __init__(self,x,y, val):
 		self.position = (x,y)
+		self.value = val
+
+class Dimensioned_Node(Node):
+	def __init__(x, y, val, dim):
+		Node.__init__(x, y, val)
+		self.dim = dim
+
 
 class Edge(object):
-	def __init__(self, start, end, cost):
+	def __init__(self, start, end, cost=1):
 		self.start = start
 		self.end = end
 		self.cost = cost
@@ -20,6 +27,52 @@ class Graph(object):
 
 	def add_edge(self, edge):
 		self.edges[edge.start].append(edge)
+
+	def __str__(self):
+		p = dict()
+		for node in self.nodes:
+			p[node.position] = []
+			for edge in self.edges[node]:
+				p[node.position].append(edge.end.position)
+		for node in p:
+			print(node, ':', p[node])
+
+		return ''
+
+	def get_neighbor_coords(self,coord):
+		x = coord.position[0]
+		y = coord.position[1]
+
+		neighbors = {(x-1, y-1), (x-1, y), (x-1, y+1),
+					(x, y-1),(x, y+1),
+					(x+1, y-1),(x+1, y),(x+1, y+1)}
+
+		return neighbors
+
+	def build_map(self, map):
+		x_max = map.shape[0]
+		y_max = map.shape[1]
+
+		for x in range(x_max):
+			for y in range(y_max):
+				pos = Node(x, y, map[x, y])
+				self.add_node(pos)
+				for coord in self.get_neighbor_coords(pos):
+					if 0 <= coord[0] <= x_max and 0 <= coord[1] <= y_max and map[x,y] != 1:
+						ed = Edge(pos, Node(coord[0], coord[1],map[x, y]))
+						self.add_edge(ed)
+
+	def build_consolidated_map(self, map):
+
+		def medium_square(coor):
+			pass
+
+		def large_square(coor):
+			pass
+
+			
+		x_max = map.shape[0]
+		y_max = map.shape[1]
 
 	def heuristic(self, node1, node2):
 		point1 = node1.position
---
+++ src/graph_builder.py
@@ -0,0 +1,136 @@
+import rospy
+from rtree import index
+from nav_msgs.msg import OccupancyGrid
+import numpy as np
+import dubins
+import tf
+from sensor_msgs.msg import PointCloud
+from geometry_msgs.msg import Point32, PoseWithCovarianceStamped, PoseStamped, Pose, Quaternion, Point
+from nav_msgs.msg import Path
+from std_msgs.msg import Header
+import scipy.misc
+import matplotlib.pyplot as plt
+import graph
+
+class search_algorithm:
+    """
+    RRT* with path smoothing
+    """
+    def __init__(self):
+        """
+        start = [x, y, theta]
+        goal = [x, y]
+        """
+        # initialize start and goal parameters
+        self.START_TOPIC = rospy.get_param("~start_topic")
+        self.GOAL_TOPIC = rospy.get_param("~goal_topic")
+        self.start_pose = [0, 0, 0] # x, y, theta
+        self.goal_pose = [0, 0, 0] # x, y, theta
+        self.goal_size = rospy.get_param("~goal_size")
+        self.map_topic = rospy.get_param("~map_topic")
+        self.goal_region = {"xmin": 0, "xmax": 0, "ymin": 0, "ymax": 0} # setup in set_goal
+        self.map_res = rospy.get_param("~map_res")
+        self.max_angle = rospy.get_param("~max_angle")
+        self.PARTICLE_CLOUD_TOPIC = rospy.get_param("~particle_cloud")
+        self.PATH_TOPIC = rospy.get_param("~path_topic")
+        self.base_link = rospy.get_param("~base_link")
+        self.map_name = rospy.get_param("~map")
+        self.origin_x_offset = rospy.get_param("~origin_x_offset")
+        self.origin_y_offset = rospy.get_param("~origin_y_offset")
+
+        #Initialize visualization varibles
+        self.cloud = PointCloud()
+
+        # initialize algorithm parameters
+        # self.max_iter = rospy.get_param("~max_iter")
+        # self.epsilon = rospy.get_param("~epsilon")
+        # self.neighbor_radius = rospy.get_param("~neighbor_radius")
+        # self.d = rospy.get_param("~d")
+        # self.turning_radius = rospy.get_param("~turning_radius")
+        # self.path_step = rospy.get_param("~path_step")
+        # self.buff_factor = rospy.get_param("~buff_map")
+        # self.counter = 0
+
+        # initilize graph structure (start insertion in set_start)
+        self.current = [0, 0, 0]
+        self.nodes = []
+        self.tree = index.Index() # R-Tree for querying neighbors
+        self.end_node = None
+
+        self.map_loaded = False
+        self.map_graph = None
+
+        # initialize publishers and subscribers
+        self.particle_cloud_publisher = rospy.Publisher(self.PARTICLE_CLOUD_TOPIC, PointCloud, queue_size=10)
+        self.path_publisher = rospy.Publisher(self.PATH_TOPIC, Path, queue_size=10)
+
+
+        rospy.Subscriber(self.START_TOPIC, PoseWithCovarianceStamped, self.set_start)
+        rospy.Subscriber(self.GOAL_TOPIC, PoseStamped, self.set_goal)
+
+        rospy.Subscriber(
+                self.map_topic,
+                OccupancyGrid,
+                self.map_callback,
+                queue_size=1)
+
+
+    def map_callback(self, map_msg):
+        # while self.start_pose == [0, 0, 0] or self.goal_pose == [0, 0, 0]:
+        #     continue
+
+        if self.map_loaded: 
+            continue
+
+        print "Loading map:", rospy.get_param("~map"), "..."
+        print "Start and Goal intialized:"
+        print "Start: ", self.start_pose
+        print "Goal: ", self.goal_pose
+        # Convert the map to a numpy array
+        map_ = np.array(map_msg.data, np.double)
+        map_ = np.clip(map_, 0, 1)
+        self.map = np.reshape(map_, (map_msg.info.height, map_msg.info.width)).T
+        # self.map = np.flip(self.map, axis=0)
+        self.map_copy = np.copy(self.map)
+        #Beef up the edges
+        for i in range(self.map_copy.shape[0]):
+            for j in range(self.map_copy.shape[1]):
+                if self.map_copy[i, j] != 0:
+                    self.map[i-self.buff_factor: i+self.buff_factor, j-self.buff_factor: j+self.buff_factor] = 1.0
+        # Convert the origin to a tuple
+        # plt.imshow(self.map)
+        # plt.show()
+        origin_p = map_msg.info.origin.position
+        origin_o = map_msg.info.origin.orientation
+        origin_o = tf.transformations.euler_from_quaternion((
+                origin_o.x,
+                origin_o.y,
+                origin_o.z,
+                origin_o.w))
+        self.origin = (origin_p.x+self.origin_x_offset, origin_p.y+self.origin_y_offset, origin_o[2])
+        if self.map_name == "stata_basement":
+            self.full_region = {
+                                "xmin": -map_msg.info.width*self.map_res + float(self.origin[0]),
+                                "xmax": float(self.origin[0]),
+                                "ymin": -map_msg.info.height*self.map_res + float(self.origin[1]),
+                                "ymax": float(self.origin[1])
+                                }
+            self.map_flip_const = -1.
+        if self.map_name == "building_31":
+            self.full_region = {
+                                "xmin": float(self.origin[0]),
+                                "xmax": map_msg.info.width*self.map_res + float(self.origin[0]),
+                                "ymin": float(self.origin[1]),
+                                "ymax": map_msg.info.height*self.map_res + float(self.origin[1])
+                                }
+            self.map_flip_const = 1.
+
+        #map is an array of zeros and ones, convert into graph
+        self.map_graph = Graph()
+        self.map_graph.build_map(self.map)
+        
+        #convert map so that large empty sels are consolidated
+        
+
+
+        self.map_loaded = True
